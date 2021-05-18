ba58ce5fd499bf86c156828e4c98d32b4a5ef6cb
James Trimble <jtrim.ons@gmail.com>
2021-05-18 16:15:25 +0100

    Move data sections out of App.svelte

---
+++ src/DataSection.svelte
@@ -0,0 +1,385 @@
+<script>
+    export let section;
+</script>
+<section class="mb-12" id="population">
+    <header>
+        <h2 class="text-2xl font-bold">Population</h2>
+    </header>
+    <div class="mb-8">
+        <table class="mb-4 border-b-2 table-auto text-base leading-loose">
+            <thead class="border-grey1 border-b-2">
+                <tr>
+                    <th scope="col" class="h-12 text-left font-bold">Resident population</th>
+                    <th scope="col" class="h-12 text-right font-normal">Count</th>
+                    <th scope="col" class="h-12 text-right font-normal" style="width: 10%">%</th>
+                </tr>
+            </thead>
+            <tbody class="border-t-2">
+                <tr class="h-9 p-0 border-grey1 font-bold border-b">
+                    <th scope="row" class="h-9 p-0 text-left font-bold">All usual residents</th>
+                    <td class="p-0 td-number">53,012,456</td>
+                    <td class="p-0 td-object"/></tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Males</td>
+                    <td class="p-0 td-number">26,069,148</td>
+                    <td class="p-0 td-number">49.2</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Females</td>
+                    <td class="p-0 td-number">26,943,308</td>
+                    <td class="p-0 td-number">50.8</td>
+                </tr>
+            </tbody>
+        </table>
+        <a href="#" class="text-base">Download this dataset</a>
+    </div>
+    <div class="mb-8">
+        <table class="mb-4 border-b-2 table-auto text-base leading-loose">
+            <thead class="border-grey1 border-b-2">
+                <tr>
+                    <th scope="col" class="h-12 text-left font-bold">Age structure</th>
+                    <th scope="col" class="h-12 text-right font-normal">Count</th>
+                    <th scope="col" class="h-12 text-right font-normal" style="width: 10%">%</th>
+                </tr>
+            </thead>
+            <tbody class="border-t-2">
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Age 0 to 4</td>
+                    <td class="p-0 td-number">3,318,449</td>
+                    <td class="p-0 td-number">6.3</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Age 5 to 7</td>
+                    <td class="p-0 td-number">1,827,610</td>
+                    <td class="p-0 td-number">3.4</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Age 8 to 9</td>
+                    <td class="p-0 td-number">1,145,022</td>
+                    <td class="p-0 td-number">2.2</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Age 10 to 14</td>
+                    <td class="p-0 td-number">3,080,929</td>
+                    <td class="p-0 td-number">5.8</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Age 15</td>
+                    <td class="p-0 td-number">650,826</td>
+                    <td class="p-0 td-number">1.2</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Age 16 to 17</td>
+                    <td class="p-0 td-number">1,314,124</td>
+                    <td class="p-0 td-number">2.5</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Age 18 to 19</td>
+                    <td class="p-0 td-number">1,375,315</td>
+                    <td class="p-0 td-number">2.6</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Age 20 to 24</td>
+                    <td class="p-0 td-number">3,595,321</td>
+                    <td class="p-0 td-number">6.8</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Age 25 to 29</td>
+                    <td class="p-0 td-number">3,650,881</td>
+                    <td class="p-0 td-number">6.9</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Age 30 to 44</td>
+                    <td class="p-0 td-number">10,944,271</td>
+                    <td class="p-0 td-number">0.6</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Age 45 to 59</td>
+                    <td class="p-0 td-number">10,276,902</td>
+                    <td class="p-0 td-number">9.4</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Age 60 to 64</td>
+                    <td class="p-0 td-number">3,172,277</td>
+                    <td class="p-0 td-number">6</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Age 65 to 74</td>
+                    <td class="p-0 td-number">4,552,283</td>
+                    <td class="p-0 td-number">8.6</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Age 75 to 84</td>
+                    <td class="p-0 td-number">2,928,118</td>
+                    <td class="p-0 td-number">5.5</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Age 85 to 89</td>
+                    <td class="p-0 td-number">776,311</td>
+                    <td class="p-0 td-number">1.5</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Age 90 and over</td>
+                    <td class="p-0 td-number">403,817</td>
+                    <td class="p-0 td-number">0.8</td>
+                </tr>
+            </tbody>
+        </table>
+        <a href="#" class="text-base">Download this dataset</a>
+    </div>
+    <div class="mb-8">
+        <table class="mb-4 border-b-2 table-auto text-base leading-loose">
+            <thead class="border-grey1 border-b-2">
+                <tr>
+                    <th scope="col" class="h-12 text-left font-bold">Ethnic group</th>
+                    <th scope="col" class="h-12 text-right font-normal">Count</th>
+                    <th scope="col" class="h-12 text-right font-normal" style="width: 10%">%</th>
+                </tr>
+            </thead>
+            <tbody class="border-t-2">
+                <tr class="h-9 p-0 border-grey1 font-bold border-b">
+                    <th scope="row" class="h-9 p-0 text-left font-bold">White
+                        
+                    
+                        
+                            </th>
+                    <td class="p-0 td-number">45,281,142</td>
+                    <td class="p-0 td-number">85.4</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">English/Welsh/Scottish/Northern Irish/British</td>
+                    <td class="p-0 td-number">42,279,236</td>
+                    <td class="p-0 td-number">79.8</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Irish</td>
+                    <td class="p-0 td-number">517,001</td>
+                    <td class="p-0 td-number">1</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Gypsy or Irish Traveller</td>
+                    <td class="p-0 td-number">54,895</td>
+                    <td class="p-0 td-number">0.1</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Other White</td>
+                    <td class="p-0 td-number">2,430,010</td>
+                    <td class="p-0 td-number">4.6</td>
+                </tr>
+            </tbody>
+            <tbody class="border-t-2">
+                <tr class="h-9 p-0 border-grey1 font-bold border-b">
+                    <th scope="row" class="h-9 p-0 text-left font-bold">Mixed/multiple ethnic groups
+                        
+                    
+                        
+                            </th>
+                    <td class="p-0 td-number">1,192,879</td>
+                    <td class="p-0 td-number">2.3</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Mixed/multiple ethnic groups</td>
+                    <td class="p-0 td-number">1,192,879</td>
+                    <td class="p-0 td-number">2.3</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">White and Black Caribbean</td>
+                    <td class="p-0 td-number">415,616</td>
+                    <td class="p-0 td-number">0.8</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">White and Black African</td>
+                    <td class="p-0 td-number">161,550</td>
+                    <td class="p-0 td-number">0.3</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">White and Asian</td>
+                    <td class="p-0 td-number">332,708</td>
+                    <td class="p-0 td-number">0.6</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Other Mixed</td>
+                    <td class="p-0 td-number">283,005</td>
+                    <td class="p-0 td-number">0.5</td>
+                </tr>
+            </tbody>
+            <tbody class="border-t-2">
+                <tr class="h-9 p-0 border-grey1 font-bold border-b">
+                    <th scope="row" class="h-9 p-0 text-left font-bold">Asian/Asian British
+                        
+                    
+                        
+                            </th>
+                    <td class="p-0 td-number">4,143,403</td>
+                    <td class="p-0 td-number">7.8</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Indian</td>
+                    <td class="p-0 td-number">1,395,702</td>
+                    <td class="p-0 td-number">2.6</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Pakistani</td>
+                    <td class="p-0 td-number">1,112,282</td>
+                    <td class="p-0 td-number">2.1</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Bangladeshi</td>
+                    <td class="p-0 td-number">436,514</td>
+                    <td class="p-0 td-number">0.8</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Chinese</td>
+                    <td class="p-0 td-number">379,503</td>
+                    <td class="p-0 td-number">0.7</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Other Asian</td>
+                    <td class="p-0 td-number">819,402</td>
+                    <td class="p-0 td-number">1.5</td>
+                </tr>
+            </tbody>
+            <tbody class="border-t-2">
+                <tr class="h-9 p-0 border-grey1 font-bold border-b">
+                    <th scope="row" class="h-9 p-0 text-left font-bold">Black/African/Caribbean/Black British
+                        
+                    
+                        
+                            </th>
+                    <td class="p-0 td-number">1,846,614</td>
+                    <td class="p-0 td-number">3.5</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">African</td>
+                    <td class="p-0 td-number">977,741</td>
+                    <td class="p-0 td-number">1.8</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Caribbean</td>
+                    <td class="p-0 td-number">591,016</td>
+                    <td class="p-0 td-number">1.1</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Other Black</td>
+                    <td class="p-0 td-number">277,857</td>
+                    <td class="p-0 td-number">0.5</td>
+                </tr>
+            </tbody>
+            <tbody class="border-t-2">
+                <tr class="h-9 p-0 border-grey1 font-bold border-b">
+                    <th scope="row" class="h-9 p-0 text-left font-bold">Other ethnic group
+                        
+                    
+                        
+                            </th>
+                    <td class="p-0 td-number">548,418</td>
+                    <td class="p-0 td-number">1</td>
+                </tr>
+            </tbody>
+            <tbody class="border-t-2">
+                <tr class="h-9 p-0 border-grey1 font-bold border-b">
+                    <th scope="row" class="h-9 p-0 text-left font-bold">Arab
+                        
+                    
+                        
+                            </th>
+                    <td class="p-0 td-number">220,985</td>
+                    <td class="p-0 td-number">0.4</td>
+                </tr>
+            </tbody>
+            <tbody class="border-t-2">
+                <tr class="h-9 p-0 border-grey1 font-bold border-b">
+                    <th scope="row" class="h-9 p-0 text-left font-bold">Any other ethnic group
+                        
+                    
+                        
+                            </th>
+                    <td class="p-0 td-number">327,433</td>
+                    <td class="p-0 td-number">0.6</td>
+                </tr>
+            </tbody>
+        </table>
+        <a href="#" class="text-base">Download this dataset</a>
+    </div>
+    <div class="mb-8">
+        <table class="mb-4 border-b-2 table-auto text-base leading-loose">
+            <thead class="border-grey1 border-b-2">
+                <tr>
+                    <th scope="col" class="h-12 text-left font-bold">Religion</th>
+                    <th scope="col" class="h-12 text-right font-normal">Count</th>
+                    <th scope="col" class="h-12 text-right font-normal" style="width: 10%">%</th>
+                </tr>
+            </thead>
+            <tbody class="border-t-2">
+                <tr class="h-9 p-0 border-grey1 font-bold border-b">
+                    <th scope="row" class="h-9 p-0 text-left font-bold">Has religion
+                        
+                    
+                        
+                            </th>
+                    <td class="p-0 td-number">3,318,449</td>
+                    <td class="p-0 td-number">68.1</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Christian</td>
+                    <td class="p-0 td-number">1,827,610</td>
+                    <td class="p-0 td-number">59.4</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Buddhist</td>
+                    <td class="p-0 td-number">1,145,022</td>
+                    <td class="p-0 td-number">0.5</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Hindu</td>
+                    <td class="p-0 td-number">3,080,929</td>
+                    <td class="p-0 td-number">1.5</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Jewish</td>
+                    <td class="p-0 td-number">650,826</td>
+                    <td class="p-0 td-number">0.5</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Muslim</td>
+                    <td class="p-0 td-number">1,314,124</td>
+                    <td class="p-0 td-number">5</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Sikh</td>
+                    <td class="p-0 td-number">1,375,315</td>
+                    <td class="p-0 td-number">0.8</td>
+                </tr>
+                <tr class="h-9 p-0 border-grey3 border-b">
+                    <td class="p-0 td-string">Other religion</td>
+                    <td class="p-0 td-number">3,595,321</td>
+                    <td class="p-0 td-number">0.4</td>
+                </tr>
+            </tbody>
+            <tbody class="border-t-2">
+                <tr class="h-9 p-0 border-grey1 font-bold border-b">
+                    <th scope="row" class="h-9 p-0 text-left font-bold">No religion
+                        
+                    
+                        
+                            </th>
+                    <td class="p-0 td-number">3,650,881</td>
+                    <td class="p-0 td-number">24.7</td>
+                </tr>
+            </tbody>
+            <tbody class="border-t-2">
+                <tr class="h-9 p-0 border-grey1 font-bold border-b">
+                    <th scope="row" class="h-9 p-0 text-left font-bold">Religion not stated
+                        
+                    
+                        
+                            </th>
+                    <td class="p-0 td-number">10,944,271</td>
+                    <td class="p-0 td-number">7.2</td>
+                </tr>
+            </tbody>
+        </table>
+        <a href="#" class="text-base">Download this dataset</a>
+    </div>
+    <a class="text-base icon-arrow p-3 pl-2 border border-nightBlue border-opacity-30  leading-none" href="#top">Back to top</a>
+</section>
